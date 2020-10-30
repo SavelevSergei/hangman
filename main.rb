59006800612b0f381b41768c98a4a0f2ccc2fe82
Sergei_Savelev <lavrentiy83@gmail.com>
2020-10-30 23:07:23 +0300

    init commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'lib/console_interface'
+require_relative 'lib/game'
+
+puts 'Привет! Поиграем!!!'
+word = File.readlines(__dir__ + '/data/words.txt', chomp: true).sample
+game = Game.new(word)
+console_interface = ConsoleInterface.new(game)
+
+until game.over?
+  console_interface.print_out
+  letter = console_interface.get_input
+  game.play!(letter)
+end
+
+console_interface.print_out
